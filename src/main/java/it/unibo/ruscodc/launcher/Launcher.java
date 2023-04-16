083c5bd313b51f65e7484af0ac91312d4732472e
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2023-04-16 18:20:54 +0200

    Add launcher class

---
+++ src/main/java/it/unibo/ruscodc/launcher/Launcher.java
@@ -0,0 +1,28 @@
+package it.unibo.ruscodc.launcher;
+
+import it.unibo.ruscodc.controller.GameControllerImpl;
+import it.unibo.ruscodc.controller.GameObserverController;
+import it.unibo.ruscodc.view.GameView;
+import it.unibo.ruscodc.view.ViewJFX;
+import javafx.application.Application;
+
+/**
+ *
+ */
+public final class Launcher {
+    private Launcher() {
+    }
+    /**
+     *
+     * @param args
+     */
+    public static void main(final String[] args) {
+        GameObserverController ctrl = new GameControllerImpl();
+        ctrl.init();
+        //ctrl.start();
+
+        // TMP:
+        Application.launch(ViewJFX.class);
+
+    }
+}
