6673f44c2aa7c51c694c1cdc7e4ecf7cb9001992
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-04-16 16:23:29 +0200

    Create basic interface for C and V

---
+++ src/main/java/it/unibo/ruscodc/controller/GameController.java
@@ -0,0 +1,36 @@
+package it.unibo.ruscodc.controller;
+
+public interface GameController {
+    /**
+     * 
+     */
+    void save();
+    /**
+     * 
+     */
+    void pause();
+    /**
+     * 
+     */
+    void resume();
+    /**
+     * 
+     */
+    void changeAutomaticSave();
+    /**
+     * 
+     */
+    void computeInput();
+    /**
+     * 
+     */
+    void quit();
+    /**
+     * 
+     */
+    void init();
+    /**
+     * 
+     */
+    void start();
+}
---
+++ src/main/java/it/unibo/ruscodc/view/GameView.java
@@ -0,0 +1,11 @@
+package it.unibo.ruscodc.view;
+
+public interface GameView {
+    void start();
+
+    //void setObserver();
+
+    //void uptade();
+
+    void printError(String err);
+}
