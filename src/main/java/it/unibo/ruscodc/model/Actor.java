84b7ea16382e5e827e8d632c95b1faff7c235c8f
Leonardo_Marcaccio <leonardo.marcaccio@studio.unibo.it>
2023-04-16 17:03:06 +0200

    Change Actor from an Interface to an Avstract class

--- src/main/java/it/unibo/ruscodc/model/Actor.java
+++ src/main/java/it/unibo/ruscodc/model/Actor.java
@@ -1,9 +1,23 @@
 package it.unibo.ruscodc.model;
 
-import javafx.util.Pair;
+import it.unibo.ruscodc.utils.Pair;
 
-public interface Actor extends Entity {
-    Pair<Integer, Integer> getPos();
-    void act();
-    void setPos(Pair<Integer, Integer> newPos);
+public abstract class Actor implements Entity {
+    private Pair<Integer, Integer> currentPos;
+    private Skills skills;
+
+    public Actor(Pair<Integer, Integer> initialPos, Skills actorSkill) {
+        this.currentPos = initialPos;
+        this.skills = actorSkill;
+    }
+
+    final public Pair<Integer, Integer> getPos() {
+        return this.currentPos;
+    };
+
+    final public void setPos(Pair<Integer, Integer> newPos) {
+        this.currentPos = newPos;
+    };
+
+    public abstract void act();
 }
