04ef28c2ae04cb78b6e370225c49bb9489877bfb
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2023-04-23 18:03:41 +0200

    Update Tile to extend Entity

--- src/main/java/it/unibo/ruscodc/model/gamemap/BaseTileImpl.java
+++ src/main/java/it/unibo/ruscodc/model/gamemap/BaseTileImpl.java
@@ -5,8 +5,9 @@ import it.unibo.ruscodc.utils.Pair;
 import it.unibo.ruscodc.view.AbstractJFXDrawableImpl;
 
 import java.util.Optional;
+import java.util.Random;
 
-public class BaseTileImpl implements Tile {
+public class BaseTileImpl implements Tile, Entity {
     private final Pair<Integer, Integer> position;
     private boolean isAccessible = true;
     private Entity content = null;
@@ -16,7 +17,7 @@ public class BaseTileImpl implements Tile {
      * @param position the position of the tile in the room
      * @param accessibility whether the tile can be accessed or not by the player
      */
-    public BaseTileImpl(Pair<Integer, Integer> position, boolean accessibility) {
+    public BaseTileImpl(final Pair<Integer, Integer> position, final boolean accessibility) {
         this.position = position;
         this.isAccessible = accessibility;
     }
@@ -32,7 +33,7 @@ public class BaseTileImpl implements Tile {
     }
 
     @Override
-    public boolean put(Entity obj) {
+    public boolean put(final Entity obj) {
         if (this.content != null)
             return false;
 
@@ -50,5 +51,29 @@ public class BaseTileImpl implements Tile {
         Optional<Entity> content = this.get();
         this.content = null;
         return content;
+    }
+
+    @Override
+    public String getInfo() {
+        Random rnd = new Random();
+        return switch (rnd.nextInt(0, 5)) {
+            case 0 -> "This floor is so dirty!";
+            case 1 -> "Nothing to see here.";
+            case 2 -> "Dust bunnies where are you?";
+            case 3 -> "I wonder where the best trash is...";
+            case 4 -> "No banana peels here! The path is clear!";
+            default -> "Just some tiles...";
+        };
+    }
+
+    @Override
+    public String getPath() {
+        // TODO: add image resource
+        return "file:src/main/resources/it/unibo/ruscodc";
+    }
+
+    @Override
+    public Pair<Integer, Integer> getPos() {
+        return this.position;
     }
 }
