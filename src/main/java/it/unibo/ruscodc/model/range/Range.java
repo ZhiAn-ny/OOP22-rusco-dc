1002d5b44146da64041c0f68200390568055200d
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-04-24 20:00:02 +0200

    little fix

--- src/main/java/it/unibo/ruscodc/model/range/Range.java
+++ src/main/java/it/unibo/ruscodc/model/range/Range.java
@@ -1,8 +1,13 @@
 package it.unibo.ruscodc.model.range;
 
+import java.util.Iterator;
+
+import it.unibo.ruscodc.model.Entity;
 import it.unibo.ruscodc.utils.Pair;
 
 public interface Range {
     
-    boolean isInRange(Pair<Integer, Integer> p);
+    boolean isInRange(Pair<Integer, Integer> by, Pair<Integer, Integer> toCheck);
+
+    Iterator<Entity> getRange(Pair<Integer, Integer> by);
 }
