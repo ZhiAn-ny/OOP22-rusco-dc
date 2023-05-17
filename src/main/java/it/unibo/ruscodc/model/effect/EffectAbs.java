f4da7d8a5292a3c233f46d71e65358c03d7026ee
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-05-17 17:48:51 +0200

    Refactor "AttackEffect" into "EffectAbs"
    let implements method "applyEffect" in other anonymous classes

--- src/main/java/it/unibo/ruscodc/model/effect/EffectAbs.java
+++ src/main/java/it/unibo/ruscodc/model/effect/EffectAbs.java
@@ -3,23 +3,21 @@ package it.unibo.ruscodc.model.effect;
 import it.unibo.ruscodc.model.actors.Actor;
 import it.unibo.ruscodc.model.actors.StatImpl.StatName;
 
-public class AttackEffect implements Effect {
+public abstract class EffectAbs implements Effect {
 
     private final int cost;
 
-    public AttackEffect(int cost){
+    public EffectAbs(int cost){
         this.cost = cost;
-    }
-
-    @Override
-    public void applyEffect(Actor from, Actor to) {
-        final int damage = from.getStatInfo(StatName.DMG);
-        to.modifyStat(StatName.HP, - damage);
     }
 
     @Override
     public int getAPcost() {
         return cost;
     }
+
+    @Override
+    public abstract void applyEffect(Actor from, Actor to);
+  
     
 }
