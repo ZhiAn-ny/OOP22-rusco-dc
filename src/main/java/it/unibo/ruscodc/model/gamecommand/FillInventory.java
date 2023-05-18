43df9d83ac9393c59610d331d03b073768735c92
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-05-18 23:50:26 +0200

    Little change into FillInventory
    (this class was extending by an old classes)

--- src/main/java/it/unibo/ruscodc/model/gamecommand/FillInventory.java
+++ src/main/java/it/unibo/ruscodc/model/gamecommand/FillInventory.java
@@ -1,11 +1,12 @@
 package it.unibo.ruscodc.model.gamecommand;
 
+import it.unibo.ruscodc.model.gamecommand.quickcommand.QuickActionAbs;
 import it.unibo.ruscodc.model.item.Item;
 import it.unibo.ruscodc.utils.exception.ModelException;
 
 import java.util.Set;
 
-public abstract class FillInventory extends QuickActionBuilder{
+public abstract class FillInventory extends QuickActionAbs {
 
     private final Set<Item> items;
     public FillInventory(Set<Item> itemSet){
