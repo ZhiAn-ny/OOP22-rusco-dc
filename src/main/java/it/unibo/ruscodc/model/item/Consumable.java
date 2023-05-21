0da4ccc12f29a327cfb313945b601d7503a7e5ab
Leonardo Marcaccio <marcaccio.leonardo@studio.unibo.it>
2023-05-21 18:55:27 +0200

    Creation of ConsumableFactory and it's partial implementation

--- src/main/java/it/unibo/ruscodc/model/item/Consumable.java
+++ src/main/java/it/unibo/ruscodc/model/item/Consumable.java
@@ -1,5 +1,7 @@
 package it.unibo.ruscodc.model.item;
 
+import it.unibo.ruscodc.model.effect.SingleTargetEffect;
+
 public interface Consumable extends Item{
-    void consume();
+    SingleTargetEffect consume();
 }
--- src/main/java/it/unibo/ruscodc/model/item/ConsumableFactoryImpl.java
+++ src/main/java/it/unibo/ruscodc/model/item/ConsumableFactoryImpl.java
@@ -1,21 +1,39 @@
 package it.unibo.ruscodc.model.item;
 
+import it.unibo.ruscodc.model.actors.Actor;
 import it.unibo.ruscodc.model.actors.stat.StatImpl.StatName;
 import it.unibo.ruscodc.model.effect.SingleTargetEffect;
 import it.unibo.ruscodc.model.gamecommand.GameCommand;
+import it.unibo.ruscodc.model.interactable.Drop;
+import it.unibo.ruscodc.model.outputinfo.InfoPayload;
+import it.unibo.ruscodc.model.outputinfo.InfoPayloadImpl;
 
 public class ConsumableFactoryImpl implements ConsumableFactory {
-
+    
     @Override
     public Consumable createHPPotion() {
         return new Consumable() {
-
+            
             // private final InfoPayload info = new InfoPayload();
+            private final String name = "Health Potion";
+            private final String path = "file:src/main/resources/it/unibo/ruscodc/map_res/HP_Potion";
+            
+            @Override
+            public String getName() {
+                return this.name;
+            }
+            
+            @Override
+            public String getPath() {
+               return this.path;
+            }
 
             @Override
             public InfoPayload getInfo() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'getInfo'");
+                return new InfoPayloadImpl(
+                    name,
+                    "A standard healing potion that heals 20% of the user HP",
+                    path);
             }
 
             @Override
@@ -24,16 +42,23 @@ public class ConsumableFactoryImpl implements ConsumableFactory {
             }
 
             @Override
-            public GameCommand drop() {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'drop'");
+            public GameCommand drop(Actor actor) {
+                return null;    // Aspetta JP
             }
 
             @Override
             public SingleTargetEffect consume() {
-                return 
+                return new SingleTargetEffect() {
+
+                    @Override
+                    public void applyEffect(Actor target) {
+                        target.modifyStat(
+                            StatName.HP,
+                            target.getStatActual(StatName.HP) + (target.getStatActual(StatName.HP)*20/100));
+                    }
+                    
+                };
             }
-            
         };
     }
 
