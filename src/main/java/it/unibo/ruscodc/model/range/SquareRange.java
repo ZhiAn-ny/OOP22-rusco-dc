d1a2c07ceb75f5ebd92ddc2aadf42041eb939dc4
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-05-22 21:29:37 +0200

    Add SquareRange class

---
+++ src/main/java/it/unibo/ruscodc/model/range/SquareRange.java
@@ -0,0 +1,34 @@
+package it.unibo.ruscodc.model.range;
+
+import java.util.stream.Stream;
+
+import it.unibo.ruscodc.utils.Pair;
+import it.unibo.ruscodc.utils.Pairs;
+
+/**
+ * Specific a Range with a square shape.
+ */
+public class SquareRange extends DecoratedRange {
+
+    private final int offset;
+
+    /**
+     * Add a square shape to the previous range.
+     * @param offset the orthogonal distance between center of square and their side
+     * @param start the previous range.
+     */
+    protected SquareRange(final int offset, final Range start) {
+        super(start);
+        this.offset = offset;
+    }
+
+    /**
+     * 
+     */
+    @Override
+    protected Stream<Stream<Pair<Integer, Integer>>> uploadShapeDelta(Pair<Integer, Integer> from,
+            Pair<Integer, Integer> to) {
+        return Pairs.computeSquare(to, offset, false);
+    }
+    
+}
