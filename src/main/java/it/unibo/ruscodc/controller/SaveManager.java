32c7f8192c9d0b3a0fa22e4c2bd7a86f24957cd6
Luca Bandiera <luca.bandiera3@studio.unibo.it>
2023-05-28 22:19:35 +0200

    Modify SaveManager and SaveTest

--- src/main/java/it/unibo/ruscodc/controller/SaveManager.java
+++ src/main/java/it/unibo/ruscodc/controller/SaveManager.java
@@ -2,6 +2,10 @@ package it.unibo.ruscodc.controller;
 
 import it.unibo.ruscodc.model.GameModel;
 
+/**
+ * This interface is used to save and load game.
+ *
+ */
 public interface SaveManager {
 
     void saveGame(String filename, GameModel toSave) throws Exception;
--- src/main/java/it/unibo/ruscodc/controller/SaveManagerImpl.java
+++ src/main/java/it/unibo/ruscodc/controller/SaveManagerImpl.java
@@ -1,17 +1,33 @@
 package it.unibo.ruscodc.controller;
 
+import com.google.gson.Gson;
 import it.unibo.ruscodc.model.GameModel;
+
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class SaveManagerImpl implements SaveManager{
 
+    private GameModel gameModel;
+    private static final Gson gson = new Gson();
 
     @Override
-    public void saveGame(String filename, GameModel toSave) throws Exception {
-
+    public void saveGame(final String filename, final GameModel toSave) throws Exception {
+        String json = gson.toJson(toSave);
+        try (FileWriter writer = new FileWriter(filename)) {
+            writer.write(json);
+        }
+        System.out.println("Game saved");
     }
 
     @Override
-    public GameModel loadGame(String filename) throws Exception {
-        return null;
+    public GameModel loadGame(final String filename) throws Exception {
+        gameModel = gson.fromJson(
+                new String(Files.readAllBytes(Paths.get(filename))),
+                GameModel.class
+        );
+        System.out.println("Game loaded");
+        return gameModel;
     }
 }
