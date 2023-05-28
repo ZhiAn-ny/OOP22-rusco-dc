650c666389e78bc3285ce6487b028457b826f844
Leonardo Marcaccio <marcaccio.leonardo@studio.unibo.it>
2023-05-28 20:13:46 +0200

    Create new Monsters, new Monster's actions, new Combact and Movement behaviours

--- src/main/java/it/unibo/ruscodc/model/actors/monster/behaviour/BehaviourFactory.java
+++ src/main/java/it/unibo/ruscodc/model/actors/monster/behaviour/BehaviourFactory.java
@@ -3,7 +3,6 @@ package it.unibo.ruscodc.model.actors.monster.behaviour;
 public interface BehaviourFactory {
     Behaviour makeMeleeAggressive();
     Behaviour makeMeleeBrainless();
-    Behaviour makeMeleeShy();
-    Behaviour makeRangedAggressive();
+    Behaviour makeRangedBrainless();
     Behaviour makeRangedShy();
 }
