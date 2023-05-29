881a93d0666ee304f90d84e83140dc8fa507a1c1
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2023-05-29 15:41:13 +0200

    Refernce fxml controller in FXMLView

--- src/main/java/it/unibo/ruscodc/view/FXMLView.java
+++ src/main/java/it/unibo/ruscodc/view/FXMLView.java
@@ -2,6 +2,8 @@ package it.unibo.ruscodc.view;
 
 import it.unibo.ruscodc.controller.GameObserverController;
 import it.unibo.ruscodc.model.Entity;
+import it.unibo.ruscodc.model.gamemap.RectangleRoomImpl;
+import it.unibo.ruscodc.model.gamemap.Room;
 import it.unibo.ruscodc.model.outputinfo.InfoPayload;
 import it.unibo.ruscodc.utils.Pair;
 import javafx.application.Application;
@@ -16,7 +18,7 @@ import java.util.List;
 public class FXMLView extends Application implements GameView {
 
     private GameObserverController controller;
-
+    private MainMenuView gameView;
 
     public FXMLView(){
 
@@ -93,6 +95,11 @@ public class FXMLView extends Application implements GameView {
     public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(FXMLView.class.getResource("main-menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        this.gameView = (MainMenuView) fxmlLoader.getController();
+
+        Room r = new RectangleRoomImpl(3, 3);
+        this.gameView.setRoom(r.getTilesAsEntity(), r.getSize());
+
         stage.setMinWidth(Screen.getPrimary().getVisualBounds().getWidth()*0.5);
         stage.setMinHeight(Screen.getPrimary().getVisualBounds().getHeight()*0.5);
         stage.setFullScreen(true);
--- src/main/java/it/unibo/ruscodc/view/MainMenuView.java
+++ src/main/java/it/unibo/ruscodc/view/MainMenuView.java
@@ -1,5 +1,7 @@
 package it.unibo.ruscodc.view;
 
+import it.unibo.ruscodc.model.Entity;
+import it.unibo.ruscodc.utils.Pair;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
@@ -11,6 +13,7 @@ import javafx.beans.binding.Bindings;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainMenuView implements Initializable {
@@ -18,26 +21,32 @@ public class MainMenuView implements Initializable {
     @FXML
     private GridPane mainGrid;
     IntegerProperty unit = new SimpleIntegerProperty();
-    private static final int rows = 8;
-    private static final int cols = 8;
+    private int rows = 2;
+    private int cols = 1;
 
+    public void setRoom(List<Entity> obj1, Pair<Integer, Integer> size) {
+        this.cols = size.getX();
+        this.rows = size.getY();
 
-    @FXML
-    public void initialize(URL location, ResourceBundle resources) {
-        for (int i=0; i < rows; i++){
-            for (int j=0; j<cols; j++){
+        for (int i = 0; i < cols; i++){
+            for (int j = 0; j < rows; j++){
                 var pane = new ImageView(new Image("file:src/main/resources/it/unibo/ruscodc/map_res/FloorTile/FloorTile.jpg"));
                 this.mainGrid.sceneProperty().addListener((observable, oldScene, newScene) -> {
                     if (newScene != null){
                         unit.bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(100));
                     }
                     if (!pane.fitWidthProperty().isBound() && newScene != null){
-                        pane.fitWidthProperty().bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(cols));
-                        pane.fitHeightProperty().bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(rows));
+                        pane.fitWidthProperty().bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(rows));
+                        pane.fitHeightProperty().bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(cols));
                     }
                 });
                 this.mainGrid.add(new Pane(pane), i, j);
             }
         }
+    }
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
