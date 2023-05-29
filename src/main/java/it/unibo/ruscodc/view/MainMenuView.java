5d59b9fe7a8efa349f34e13fd8d7275f8d9100a2
Anny Bevilacqua <anny.bevilacqua@studio.unibo.it>
2023-05-29 16:47:17 +0200

    Render Tiles' images

--- src/main/java/it/unibo/ruscodc/view/MainMenuView.java
+++ src/main/java/it/unibo/ruscodc/view/MainMenuView.java
@@ -1,6 +1,7 @@
 package it.unibo.ruscodc.view;
 
 import it.unibo.ruscodc.model.Entity;
+import it.unibo.ruscodc.model.gamemap.Tile;
 import it.unibo.ruscodc.utils.Pair;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -14,9 +15,12 @@ import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class MainMenuView implements Initializable {
+    private static final int TILES_RENDERING_LEVEL = 1;
 
     @FXML
     private GridPane mainGrid;
@@ -24,15 +28,15 @@ public class MainMenuView implements Initializable {
     private int rows = 2;
     private int cols = 1;
 
-    public void setRoom(List<Entity> obj1, Pair<Integer, Integer> size) {
+    public void setRoomTMP(List<Entity> obj1, Pair<Integer, Integer> size) {
         this.cols = size.getX();
         this.rows = size.getY();
 
-        for (int i = 0; i < cols; i++){
-            for (int j = 0; j < rows; j++){
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < rows; j++) {
                 var pane = new ImageView(new Image("file:src/main/resources/it/unibo/ruscodc/map_res/FloorTile/FloorTile.jpg"));
                 this.mainGrid.sceneProperty().addListener((observable, oldScene, newScene) -> {
-                    if (newScene != null){
+                    if (newScene != null) {
                         unit.bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(100));
                     }
                     if (!pane.fitWidthProperty().isBound() && newScene != null){
@@ -43,6 +47,38 @@ public class MainMenuView implements Initializable {
                 this.mainGrid.add(new Pane(pane), i, j);
             }
         }
+    }
+
+    public void setRoom(final List<Entity> obj1, final Pair<Integer, Integer> size) {
+        this.cols = size.getX();
+        this.rows = size.getY();
+
+        for (int i = 0; i < cols+2; i++){
+            for (int j = 0; j < rows+2; j++){
+                final Optional<Entity> entity = this.getTileAtPosition(obj1, i,j);
+                if (entity.isEmpty()) {
+                    continue;
+                }
+
+                var pane = new ImageView(new Image(entity.get().getPath()+ "/Sprite.png"));
+                this.mainGrid.sceneProperty().addListener((observable, oldScene, newScene) -> {
+                    if (newScene != null) {
+                        unit.bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(100));
+                    }
+                    if (!pane.fitWidthProperty().isBound() && newScene != null){
+                        pane.fitWidthProperty().bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(rows));
+                        pane.fitHeightProperty().bind(Bindings.min(mainGrid.getScene().widthProperty(), mainGrid.getScene().heightProperty()).divide(cols));
+                    }
+                });
+                this.mainGrid.add(new Pane(pane), i, j);
+            }
+        }
+    }
+
+    private Optional<Entity> getTileAtPosition(final List<Entity> entities, final int x, final int y) {
+        return entities.stream().filter(e -> e.getID() == TILES_RENDERING_LEVEL)
+                .filter(t -> t.getPos().getX().equals(x) && t.getPos().getY().equals(y))
+                .findFirst();
     }
 
     @FXML
