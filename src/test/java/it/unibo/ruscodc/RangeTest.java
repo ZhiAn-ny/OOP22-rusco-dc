702c661008580d41b7c043a720c732238073761d
Jacopo Pesaresi <jacopo.pesaresi2@studio.unibo.it>
2023-05-31 18:36:35 +0200

    Fix code into test classes to support change done into MonsterGenerator

--- src/test/java/it/unibo/ruscodc/RangeTest.java
+++ src/test/java/it/unibo/ruscodc/RangeTest.java
@@ -66,7 +66,7 @@ final class RangeTest {
 
         final List<Pair<Integer, Integer>> positions = List.of(pos1, pos2, pos3);
 
-        positions.forEach(p -> enemys.add(mg.makeMeleeRat(counter.next() + " " + p.toString(), p)));
+        positions.forEach(p -> enemys.add(mg.makeMeleeRat(p)));
 
         enemyPos = () -> enemys.stream().map(a -> a.getPos());
 
@@ -87,7 +87,7 @@ final class RangeTest {
     void testCircle2() { 
         final Range myRange = new CircleRange(BASIC_R_SIZE, basicR);
         final Pair<Integer, Integer> farPos = new Pair<>(0, 0);
-        enemys.add(mg.makeMeleeRat(counter.next() + " " + farPos.toString(), farPos));
+        enemys.add(mg.makeMeleeRat(farPos));
         final long counted = enemyPos.get()
             .filter(ep -> myRange.isInRange(heroPos, cursorPos, ep, r)).count();
         assertEquals(3, counted, ERR_M);
@@ -105,7 +105,7 @@ final class RangeTest {
     void testglobalRange2() { 
         final Range gR = new GlobalRange(basicR);
         final Pair<Integer, Integer> farPos = new Pair<>(0, 0);
-        enemys.add(mg.makeMeleeRat(counter.next() + " " + farPos.toString(), farPos));
+        enemys.add(mg.makeMeleeRat(farPos));
         final long counted = enemyPos.get()
             .filter(ep -> gR.isInRange(heroPos, cursorPos, ep, r)).count();
         assertEquals(4, counted, ERR_M);
